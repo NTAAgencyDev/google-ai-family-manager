b93029c372f3bcaa433bb08eab8c8f13e4203de8
NTAAgencyDev <ntaagency.work@gmail.com>
2026-09-17 15:40:55 +0700

    Fix: Bỏ ảnh lỗi logo thanh toán và thêm padding để nút Zalo không che nội dung

--- js/customer.js
+++ js/customer.js
@@ -373,10 +373,6 @@ const Customer = {
             <svg viewBox="0 0 24 24" width="14" height="14" fill="none" stroke="currentColor" stroke-width="2"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path></svg>
             Thanh toán được mã hóa an toàn
           </div>
-          <div class="secure-payment-logos">
-            <img src="https://img.vietqr.io/image/vietqr.png" alt="VietQR">
-            <img src="https://img.vietqr.io/image/napas.png" alt="Napas">
-          </div>
         </div>
         
         <div class="qr-memo-box">
