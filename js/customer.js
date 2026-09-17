e7c58d1ec3865513e1afb6eb7414421009403b66
NTAAgencyDev <ntaagency.work@gmail.com>
2026-09-17 15:59:25 +0700

    Fix: Bỏ hiệu ứng 3D tilt theo yêu cầu

--- js/customer.js
+++ js/customer.js
@@ -233,24 +233,6 @@ const Customer = {
     }
     
     dash.innerHTML = html;
-    
-    // Add 3D Tilt Effect
-    const cards = dash.querySelectorAll('.package-card');
-    cards.forEach(card => {
-      card.addEventListener('mousemove', (e) => {
-        const rect = card.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
-        const rotateX = ((y - centerY) / centerY) * -5;
-        const rotateY = ((x - centerX) / centerX) * 5;
-        card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.02, 1.02, 1.02)`;
-      });
-      card.addEventListener('mouseleave', () => {
-        card.style.transform = 'perspective(1000px) rotateX(0) rotateY(0) scale3d(1, 1, 1)';
-      });
-    });
   },
   
   calculateExpiry(orderDateStr, productName) {
